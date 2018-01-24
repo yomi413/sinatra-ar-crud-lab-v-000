6e7aeb1f3762984aa357bcf2dfac9fc6ed31a6f3
Yomaira Escano <yjescano@gmail.com>
2018-01-24 20:24:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,23 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
-  end
+  # get '/posts/new' do
+  #   erb :new
+  # end
+
+  # post '/posts' do
+  #   @post = Post.create(params)
+  #   redirect to '/posts'
+  # end
+  #
+  # get '/posts' do
+  #   @posts = Post.all
+  #   erb :index
+  # end
+  #
+  # get '/posts/:id' do
+  #   @post = Post.find(params[:id])
+  #   erb :show
+  # end
+
 end
